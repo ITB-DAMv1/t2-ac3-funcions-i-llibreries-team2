8b97a20c028446de959dbba9207dc98bc2f2438f
JhonOsorio29 <jhon.osorio.7e5@itb.cat>
2024-11-14 08:20:09 +0100

    FuncionJhon

---
+++ FuncionJhonGp2/FuncionJhonGp2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace FuncionJhonGp2
+{
+    public class FuncionJhonGp2
+    {
+        public static void Main()
+        {
+            int userAvatar = 0;
+            const string MsgUserAvatar = "Tria el avatar!Has d'introduir un nombre entre 1 i 4.";
+            //Quitamos STRING Ya que userAvatar ya esta inicializado. 
+            // AvatarSelector(string userAvatar by ref, string MsgUserAvatar by ref);
+            AvatarSelector(ref userAvatar, MsgUserAvatar);
+        }
+        public static void AvatarSelector(ref int userAvatar, string MsgUserAvatar)
+        {
+            Console.WriteLine(MsgUserAvatar);
+
+            //añadidos parentesis externos para que funcione while !(userAvatar >= 1 && userAvatar <= 4)
+            while (!(userAvatar >= 1 && userAvatar <= 4))
+            {
+                userAvatar = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
